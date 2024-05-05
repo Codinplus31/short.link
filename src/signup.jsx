93cc33a124c2d5d46717fa4fcb494d520dd545ad
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-05-05 12:51:30 +0100

    Update signup.jsx

--- src/signup.jsx
+++ src/signup.jsx
@@ -27,8 +27,7 @@ return (<div className="w-full h-[24rem] flex  items-center justify-center">
     if( data.password === '' || data.password.length
      < 6 || data.password !== data.cpassword ){
         e.target[2].style.border = '1px solid red';
-      data.password.length
-     < 6  e.target[3].style.border = '1px solid red';
+       e.target[3].style.border = '1px solid red';
 
     }else{
         e.target[2].style.border = '0';
