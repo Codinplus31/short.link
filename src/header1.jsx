317dd8f91c7c44294e3f219086777f7dcbd8bf33
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-05-06 17:15:53 +0100

    Update header1.jsx

--- src/header1.jsx
+++ src/header1.jsx
@@ -7,7 +7,7 @@ function Header(){
     const [searchdisplay, setsearchdisplay] = useState(true);
     const [isMobile, setIsMobile] = useState(window.matchMedia('(max-width: 600px)').matches);
     const [istablet, setIstablet] = useState(window.matchMedia('(max-width: 837px)').matches);
-    let {slide,setslide, isLoggedin}  =  useContext(Loading);
+    let {slide,setslide, isLoggedin, setLogin}  =  useContext(Loading);
 
     let [remindlist,setremindlist] = useState(localStorage.getItem('list'))
     let [notificationlength,setnotificationlength] = useState(localStorage.getItem('notificationlength'));
@@ -56,7 +56,7 @@ function Header(){
         </div> : <div className='mr-[4%]'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" onClick={()=>{
     localStorage.removeItem('shorlink_user')
-                 
+                 setLogin(localStorage.getItem('shorlink_user')  !== null?true:false )
         }} className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
 </svg>
